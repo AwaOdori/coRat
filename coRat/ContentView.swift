298ed200ac1d806f6f4be119ce4650b2e39fde1b
AwaOdori <https://www.google.com/search?q=xcode+add+package+dependency+missing&rlz=1C5CHFA_enJP976JP976&sxsrf=APq-WBvtrCupSHiMRKj0IROwJv6zWeqDig%3A1643277141923&ei=VWvyYbXWN7-q0PEP7sOI0AU&oq=xcode+add+package+dependency+&gs_lcp=Cgdnd3Mtd2l6EAEYADIECAAQEzIECAAQEzIECAAQEzIGCAAQHhATMgYIABAeEBMyCAgAEAgQHhATMggIABAIEB4QEzIICAAQCBAeEBM6CAgAEIAEELADOgcIABCwAxAeOgkIABCwAxAIEB46BQghEKABSgQIQRgBSgQIRhgAUIoEWPQSYLkaaAJwAHgAgAGrAYgBigOSAQMxLjKYAQCgAQHIAQfAAQE&sclient=gws-wiz>
2023-08-27 00:24:41 +0900

    First commite

--- coRat/ContentView.swift
+++ coRat/ContentView.swift
@@ -13,7 +13,6 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
         }
         .padding()
     }

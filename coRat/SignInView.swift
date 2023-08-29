6373d12c739dea99efb671a86757516ff339c67b
AwaOdori <https://www.google.com/search?q=xcode+add+package+dependency+missing&rlz=1C5CHFA_enJP976JP976&sxsrf=APq-WBvtrCupSHiMRKj0IROwJv6zWeqDig%3A1643277141923&ei=VWvyYbXWN7-q0PEP7sOI0AU&oq=xcode+add+package+dependency+&gs_lcp=Cgdnd3Mtd2l6EAEYADIECAAQEzIECAAQEzIECAAQEzIGCAAQHhATMgYIABAeEBMyCAgAEAgQHhATMggIABAIEB4QEzIICAAQCBAeEBM6CAgAEIAEELADOgcIABCwAxAeOgkIABCwAxAIEB46BQghEKABSgQIQRgBSgQIRhgAUIoEWPQSYLkaaAJwAHgAgAGrAYgBigOSAQMxLjKYAQCgAQHIAQfAAQE&sclient=gws-wiz>
2023-08-29 15:39:08 +0900

    住所機能の実装,Pickerの不具合の修正を行いました

--- coRat/SignInView.swift
+++ coRat/SignInView.swift
@@ -10,12 +10,16 @@ import Foundation
 import PDFKit
 import FirebaseAuth
 import FirebaseFirestore
+import CoreLocation
 
 struct SignInView: View {
     @State var gender = ""
     @State var dateOfBirth:Date =  Calendar.current.date(from: DateComponents(year: 1990, month: 1, day: 1))!
     @State var socialAttribute=""
     @State var signInState = false
+    @State var errorMassege = ""
+    @State var address = 0
+    @State var country = "Japan"
     let genderArray = ["Female","Male","Other"]
     let socialAttributeArray = [
         "Company employee",
@@ -37,30 +41,39 @@ struct SignInView: View {
         NavigationStack{
             VStack{
                 Text("Sign in")
+                Text(errorMassege)
                 Form{
                     Section{
-                        Picker(selection: $gender, label: Text("Enter your gender")){
-                            ForEach(0 ..< genderArray.count){num in
-                                Text(self.genderArray[num]).tag(num)
+                        VStack{
+                            Picker(selection: $gender, label: Text("Enter your gender")){
+                                ForEach(genderArray, id: \.self){ item in
+                                    Text(item)
+                                }
                             }
-                        }
-                        DatePicker("Pick your date of birth",selection: $dateOfBirth,displayedComponents: .date)
-                        //個人情報{生年月日，社会的属性，住所}
-                        //同意
-                        Picker(selection: $socialAttribute, label: Text("Enter your gender")){
-                            ForEach(0 ..< socialAttributeArray.count){num in
-                                Text(self.socialAttributeArray[num]).tag(num)
+                            DatePicker("Pick your date of birth",selection: $dateOfBirth,displayedComponents: .date)
+                            //個人情報{生年月日，社会的属性，住所}
+                            //同意
+                            Picker(selection: $socialAttribute, label: Text("Enter your gender")){
+                                ForEach(socialAttributeArray, id: \.self){ item in
+                                    Text(item)
+                                }
                             }
+                            Text("If you live in Japan,Enter your ZIP Code")
+                            TextField("",value:$address,format:.number).keyboardType(.numberPad)
+                            Text("If you don't live in Japan Pick country you live in")
+                            TextField("",text:$country)
                         }
                     }
                     Section{
                         Button(action: {
                             let user = Auth.auth().currentUser
-                            if let user = user{
-                                let uid = user.uid
-                                Firestore.firestore().collection("user").document(uid).setData(["uid":uid,"dateOfBirth":dateOfBirth,"gender":gender,"socialAttribute":socialAttribute])
-                                signInState = true
-                            }
+                            if gender != "" && socialAttribute != ""{
+                                if let user = user{
+                                    let uid = user.uid
+                                    Firestore.firestore().collection("user").document(uid).setData(["uid":uid,"dateOfBirth":dateOfBirth,"gender":gender,"socialAttribute":socialAttribute])
+                                    signInState = true
+                                }
+                            }else{}
                         }){
                             Text("Sign in")
                             NavigationLink(destination: ContentView(),isActive:$signInState){}
